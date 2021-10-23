75e010f5a42e52a61ddef03642b516226fc90624
nickrandolph <nick@builttoroam.com>
2021-10-23 15:32:55 +1100

    Adding ios sample app

--- src/apps/dotnet/xamarin/XamarinApps/XamariniOSApp/Main.cs
+++ src/apps/dotnet/xamarin/XamarinApps/XamariniOSApp/Main.cs
@@ -2,13 +2,13 @@
 
 namespace XamariniOSApp;
 
-public class Application
-{
-    // This is the main entry point of the application.
-    static void Main(string[] args)
+    public class Application
     {
-        // if you want to use a different Application Delegate class from "AppDelegate"
-        // you can specify it here.
-        UIApplication.Main(args, null, typeof(AppDelegate));
+        // This is the main entry point of the application.
+        static void Main(string[] args)
+        {
+            // if you want to use a different Application Delegate class from "AppDelegate"
+            // you can specify it here.
+            UIApplication.Main(args, null, typeof(AppDelegate));
+        }
     }
-}
